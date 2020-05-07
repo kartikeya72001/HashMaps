9951e07fb0992021ddf3a614c94f683cee589453
kartikeya72001 <kartikeya72001@gmail.com>
2020-05-07 23:20:09 +0530

    New Question

---
+++ LongestStreak.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <unordered_set>
+using namespace std {
+
+} /* std */
+
+int longestStreak(int *arr, int count)
+{
+    unordered_set<int> s;
+    for (size_t i = 0; i < count; i++) {
+        s.insert(arr[i]);
+    }
+    for (size_t i = 0; i < count; i++) {
+        if (s.find(arr[i]-1) != s.end()) {
+            continue;
+        }
+        else{
+            n = 0;
+            int x = arr[i];
+            while (s.find(x) != s.end()) {
+                n++;
+                x++;
+            }
+            ans = max(ans,count);
+        }
+    }
+    return ans;
+}
+
+int main(int argc, char const *argv[]) {
+    int n;
+    cin>>n;
+    int *arr = new int[n];
+    for(int i=0;i<n;i++)
+    {
+        std::cin >> arr[i];
+    }
+    int ans = longestStreak(arr,n);
+    cout<<"Longest Streak is: "<<ans<<endl;
+    return 0;
+}
